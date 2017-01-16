4f334c3162f487ad515c95c5a54388c83b4ec6d7
Craig R Webster <craig@barkingiguana.com>
2017-01-16 17:56:56 +0000

    Hide from inspector

--- lib/barking_iguana/logging/helper.rb
+++ lib/barking_iguana/logging/helper.rb
@@ -9,6 +9,13 @@ module BarkingIguana
         def logger
           @logger ||= Logging.get_logger(self)
         end
+
+        def inspect
+          s = super
+          remove = Regexp.new(Regexp.escape("@logger=#{logger.inspect}"))
+          s.gsub! /(?:, )#{remove}/, ''
+          s
+        end
       end
     end
   end
